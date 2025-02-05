d3366a3f5fe98c66961f07426322b5751c60960e
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-05 19:43:42 +0700

    feat(core/designsystem): add icons

---
+++ core/designsystem/src/main/java/com/demo/hh/core/designsystem/icon/HhIcons.kt
@@ -0,0 +1,104 @@
+package com.demo.hh.core.designsystem.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.demo.hh.core.designsystem.R
+
+object HhIcons {
+
+    val ApplyCount
+        @Composable
+        get() = painterResource(R.drawable.ic_apply_count_16)
+
+    val ArrowBack
+        @Composable
+        get() = painterResource(R.drawable.ic_arrow_back_24)
+
+    val Bag
+        @Composable
+        get() = painterResource(R.drawable.ic_bag_16)
+
+    val ChatChecked
+        @Composable
+        get() = painterResource(R.drawable.ic_chat_checked_24)
+
+    val ChatUnchecked
+        @Composable
+        get() = painterResource(R.drawable.ic_chat_unchecked_24)
+
+    val CheckCircle
+        @Composable
+        get() = painterResource(R.drawable.ic_check_circle_16)
+
+    val HeartCheckedBubble
+        @Composable
+        get() = painterResource(R.drawable.ic_heart_checked_bubble_24)
+
+    val HeartFilled
+        @Composable
+        get() = painterResource(R.drawable.ic_heart_filled_24)
+
+    val HeartUnchecked
+        @Composable
+        get() = painterResource(R.drawable.ic_heart_unchecked_24)
+
+    val HeartUncheckedBubble
+        @Composable
+        get() = painterResource(R.drawable.ic_heart_unchecked_bubble_24)
+
+    val LetterChecked
+        @Composable
+        get() = painterResource(R.drawable.ic_letter_checked_24)
+
+    val LetterUnchecked
+        @Composable
+        get() = painterResource(R.drawable.ic_letter_unchecked_24)
+
+    val ListChecked
+        @Composable
+        get() = painterResource(R.drawable.ic_list_checked_24)
+
+    val Map
+        @Composable
+        get() = painterResource(R.drawable.ic_map_24)
+
+    val ProfileChecked
+        @Composable
+        get() = painterResource(R.drawable.ic_profile_checked_24)
+
+    val ProfileUnchecked
+        @Composable
+        get() = painterResource(R.drawable.ic_profile_unchecked_24)
+
+    val SearchChecked
+        @Composable
+        get() = painterResource(R.drawable.ic_search_checked_24)
+
+    val SearchUnchecked
+        @Composable
+        get() = painterResource(R.drawable.ic_search_unchecked_24)
+
+    val Settings
+        @Composable
+        get() = painterResource(R.drawable.ic_settings_24)
+
+    val Share
+        @Composable
+        get() = painterResource(R.drawable.ic_share_24)
+
+    val Sort
+        @Composable
+        get() = painterResource(R.drawable.ic_sort_16)
+
+    val Star
+        @Composable
+        get() = painterResource(R.drawable.ic_star_24)
+
+    val View
+        @Composable
+        get() = painterResource(R.drawable.ic_view_28)
+
+    val Count
+        @Composable
+        get() = painterResource(R.drawable.ic_view_count_16)
+}
