3ac51a163bd001788fac55ff0d498e3950782e78
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-06 19:47:20 +0700

    feat(core/ui): implement shared components

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.demo.hh.core.ui"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    api(project(":core:designsystem"))
+    api(project(":core:model"))
+    debugImplementation(libs.androidx.ui.tooling)
+}
---
+++ core/ui/src/main/java/com/demo/hh/core/ui/VacancyApplyButton.kt
@@ -0,0 +1,54 @@
+package com.demo.hh.core.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.demo.hh.core.designsystem.component.HhSecondaryLargeButton
+import com.demo.hh.core.designsystem.component.HhSecondarySmallButton
+import com.demo.hh.core.designsystem.theme.HhTheme
+
+@Composable
+fun VacancyApplyButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    HhSecondarySmallButton(
+        onClick = onClick,
+        modifier = modifier,
+        content = {
+            Text(stringResource(R.string.button_apply_for_vacancy))
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun VacancyApplyButtonPreview() {
+    HhTheme {
+        VacancyApplyButton({})
+    }
+}
+
+@Composable
+fun LargeVacancyApplyButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    HhSecondaryLargeButton(
+        onClick = onClick,
+        modifier = modifier,
+        content = {
+            Text(stringResource(R.string.button_apply_for_vacancy))
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun LargeVacancyApplyButtonPreview() {
+    HhTheme {
+        LargeVacancyApplyButton({})
+    }
+}
