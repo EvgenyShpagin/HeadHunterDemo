0c8e0971c96668562ad78d6585d6bdfe52970255
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-07 19:04:52 +0700

    feat(core/ui): add VacancyCardUiState list for preview

---
+++ core/ui/src/main/java/com/demo/hh/core/ui/util/PreviewUtils.kt
@@ -0,0 +1,73 @@
+package com.demo.hh.core.ui.util
+
+import com.demo.hh.core.ui.state.VacancyCardUiState
+
+val PreviewVacancyCardUiStateList
+    get() = listOf(
+        VacancyCardUiState(
+            id = "cbf0c984-7c6c-4ada-82da-e29dc698bb50",
+            title = "UI/UX дизайнер",
+            lookingNumber = 2,
+            salary = "Уровень дохода не указан",
+            town = "Минск",
+            company = "Мобирикс",
+            experience = "1–3 года",
+            publishDate = "2024-02-20",
+            isFavorite = false
+        ),
+        VacancyCardUiState(
+            id = "54a876a5-2205-48ba-9498-cfecff4baa6e",
+            title = "UI/UX-дизайнер / Web-дизайнер / Дизайнер интерфейсов",
+            lookingNumber = 17,
+            salary = "от 20 000 до 50 000 ₽ на руки",
+            town = "Казань",
+            company = "Шафигуллин Шакир",
+            experience = "1–3 года",
+            publishDate = "2024-03-06",
+            isFavorite = false
+        ),
+        VacancyCardUiState(
+            id = "75c84407-52e1-4cce-a73a-ff2d3ac031b3",
+            title = "UX/UI Designer",
+            lookingNumber = 0,
+            salary = "Уровень дохода не указан",
+            town = "Казань",
+            company = "Aston",
+            experience = "3–6 лет",
+            publishDate = "2024-02-28",
+            isFavorite = true
+        ),
+        VacancyCardUiState(
+            id = "16f88865-ae74-4b7c-9d85-b68334bb97db",
+            title = "Веб-дизайнер",
+            lookingNumber = 57,
+            salary = "от 60 000 ₽ до вычета налогов",
+            town = "Казань",
+            company = "Алабуга. Маркетинг и PR",
+            experience = "не требуется",
+            publishDate = "2024-03-02",
+            isFavorite = false
+        ),
+        VacancyCardUiState(
+            id = "26f88856-ae74-4b7c-9d85-b68334bb97db",
+            title = "Ведущий продуктовый дизайнер",
+            lookingNumber = 29,
+            salary = "Уровень дохода не указан",
+            town = "Минск",
+            company = "ПАО Ростелеком",
+            experience = "3–6 лет",
+            publishDate = "2024-02-19",
+            isFavorite = false
+        ),
+        VacancyCardUiState(
+            id = "15f88865-ae74-4b7c-9d81-b78334bb97db",
+            title = "Product Designer",
+            lookingNumber = 1,
+            salary = "Уровень дохода не указан",
+            town = "Минск",
+            company = "TravelLine",
+            experience = "3–6 лет",
+            publishDate = "2024-02-02",
+            isFavorite = false
+        )
+    )
