cb0d4c8a15c5e899e5744cba035a702ff469483b
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-07 22:40:08 +0700

    fix(feature/main): repair fake repo

--- feature/main/src/main/java/com/demo/hh/feature/main/data/repository/OfferRepositoryImpl.kt
+++ feature/main/src/main/java/com/demo/hh/feature/main/data/repository/OfferRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.demo.hh.feature.main.data.repository
 
+import com.demo.hh.core.common.network.FetchError
+import com.demo.hh.core.common.result.Result
 import com.demo.hh.core.model.Offer
 import com.demo.hh.feature.main.domain.repository.OfferRepository
 import com.demo.hh.feature.main.ui.main.PreviewOfferList
@@ -7,9 +9,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class OfferRepositoryImpl : OfferRepository {
-    override fun getAll(): Flow<List<Offer>> {
+    override fun getAll(): Flow<Result<List<Offer>, FetchError>> {
         return flow {
-            emit(PreviewOfferList)
+            emit(Result.Success(PreviewOfferList))
         }
     }
 }
