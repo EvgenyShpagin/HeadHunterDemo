a9d0ff6ed6e07bd28acbb695fe6e67f87d1518a1
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-07 21:49:22 +0700

    refactor(feature/main): use Result in repo

--- feature/main/src/main/java/com/demo/hh/feature/main/domain/GetOffersUseCase.kt
+++ feature/main/src/main/java/com/demo/hh/feature/main/domain/GetOffersUseCase.kt
@@ -1,11 +1,13 @@
 package com.demo.hh.feature.main.domain
 
+import com.demo.hh.core.common.network.FetchError
+import com.demo.hh.core.common.result.Result
 import com.demo.hh.core.model.Offer
 import com.demo.hh.feature.main.domain.repository.OfferRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetOffersUseCase(private val offerRepository: OfferRepository) {
-    operator fun invoke(): Flow<List<Offer>> {
+    operator fun invoke(): Flow<Result<List<Offer>, FetchError>> {
         return offerRepository.getAll()
     }
 }
--- feature/main/src/main/java/com/demo/hh/feature/main/domain/repository/OfferRepository.kt
+++ feature/main/src/main/java/com/demo/hh/feature/main/domain/repository/OfferRepository.kt
@@ -1,8 +1,10 @@
 package com.demo.hh.feature.main.domain.repository
 
+import com.demo.hh.core.common.network.FetchError
+import com.demo.hh.core.common.result.Result
 import com.demo.hh.core.model.Offer
 import kotlinx.coroutines.flow.Flow
 
 interface OfferRepository {
-    fun getAll(): Flow<List<Offer>>
+    fun getAll(): Flow<Result<List<Offer>, FetchError>>
 }
