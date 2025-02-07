20f2f866075b89027418ff5c58ed0559ef232afd
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-07 19:21:42 +0700

    feat(feature/main): add contentPadding to OfferList

--- feature/main/src/main/java/com/demo/hh/feature/main/ui/main/MainComponents.kt
+++ feature/main/src/main/java/com/demo/hh/feature/main/ui/main/MainComponents.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -112,10 +113,12 @@ private fun OfferCardPreview() {
 internal fun OfferList(
     offers: List<Offer>,
     onOfferClick: (Offer) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues()
 ) {
     LazyRow(
         modifier = modifier,
+        contentPadding = contentPadding,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(offers) { offer ->
